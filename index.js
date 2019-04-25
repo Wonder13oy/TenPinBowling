898473982fd1a1d42f3a30f369ff69481e444873
Wandile Nxumalo <46021425+Wonder13oy@users.noreply.github.com>
2019-04-25 09:35:04 +0200

    Add files via upload
    A better game architecture.

--- index.js
+++ index.js
@@ -1,150 +1,241 @@
-let game = new BowlingGame();
+let rollsArr = [];
+let p1 = new BowlingGame();
+let p2 = new BowlingGame();
 let count1 = 0;
 let count2 = 0;
-let rolls = 20;
-let sum = 0;
+let p1rolls = 2;
+let p2rolls = 2;
+let p1Score = 0;
+let p2Score = 0;
+let isStrike = false;
+let isSpare = false;
+//______________________________DO NOT DELETE THE BELOW COMMENT_______________________________________
+// p1.setPlayerName(prompt('Enter Name Player One:'));
+// p2.setPlayerName(prompt('Enter Name Player Two:'));
 
-{
-	var p1frames = [
-		(frame1 = [
-			document.getElementsByClassName('p1Frames1')[0],
-			document.getElementsByClassName('p1Frames2')[0],
-		]),
-		(frame2 = [
-			document.getElementsByClassName('p1Frames1')[1],
-			document.getElementsByClassName('p1Frames2')[1],
-		]),
-		(frame3 = [
-			document.getElementsByClassName('p1Frames1')[2],
-			document.getElementsByClassName('p1Frames2')[2],
-		]),
-		(frame4 = [
-			document.getElementsByClassName('p1Frames1')[3],
-			document.getElementsByClassName('p1Frames2')[3],
-		]),
-		(frame5 = [
-			document.getElementsByClassName('p1Frames1')[4],
-			document.getElementsByClassName('p1Frames2')[4],
-		]),
-		(frame6 = [
-			document.getElementsByClassName('p1Frames1')[5],
-			document.getElementsByClassName('p1Frames2')[5],
-		]),
-		(frame7 = [
-			document.getElementsByClassName('p1Frames1')[6],
-			document.getElementsByClassName('p1Frames2')[6],
-		]),
-		(frame8 = [
-			document.getElementsByClassName('p1Frames1')[7],
-			document.getElementsByClassName('p1Frames2')[7],
-		]),
-		(frame9 = [
-			document.getElementsByClassName('p1Frames1')[8],
-			document.getElementsByClassName('p1Frames2')[8],
-		]),
-		(frame10 = [
-			document.getElementsByClassName('p1Frames1')[9],
-			document.getElementsByClassName('p1Frames2')[9],
-		]),
-	];
+// function playGame() {
+// 	document.getElementById('playerOneName').innerText = p1.getPlayerName();
+// 	document.getElementById('playerTwoName').innerText = p2.getPlayerName();
 
-	var p2frames = [
-		(p2frame1 = []),
-		(p2frame2 = []),
-		(p2frame3 = []),
-		(p2frame4 = []),
-		(p2frame5 = []),
-		(p2frame6 = []),
-		(p2frame7 = []),
-		(p2frame8 = []),
-		(p2frame9 = []),
-		(p2frame10 = []),
-	];
-}
-
+// 	document.getElementById('buttons2').style.display = 'none';
+// 	document.getElementById('buttons1').style.display = 'inline-block';
+// 	document.getElementById('startBut').style.display = 'none';
+// }
+//______________________________DO NOT DELETE THE ABOVE COMMENT________________________________________
+// TODO: Send this funtions to your class
 function playerOneGame(pinsHit) {
-	// playerOneTurn.roll(pinHit(pinsHit));
-	// rollMany(0, --count, playerOneTurn);
-	// p1frames[frameIndex].push(playerOneTurn.score());
-	// document.getElementsByClassName('p1Frames1')[frameIndex].innerHTML =
-	// 	p1frames[frameIndex];
-
 	let pins = pinsHit;
-	let tempNum = rolls;
 
-	rollsArr.push(pins);
-	if (rollsArr.length == 2) {
-		if (rollsArr[0] + rollsArr[1] <= 10) {
-			for (var x = 0; x < 20; x++) {
-				for (var frameIndex = 0; frameIndex < 2; frameIndex++) {
-					p1frames[frameIndex].innerHTML = rollsArr[frameIndex];
-				}
+	if (count1 < 10) {
+		rollsArr.push(pins);
+		document.getElementsByClassName('p1Frames1')[count1].innerHTML =
+			rollsArr[0];
+
+		if (rollsArr.length == 2) {
+			if (isStrike) {
+				p1Score = p1Score + rollsArr[0] + rollsArr[1];
+				document.getElementById('marker' + (count1 - 1)).innerHTML = p1Score;
 			}
-			sum += pinHit(rollsArr[0], rollsArr[1]);
-			document.getElementById('marker' + count1).innerHTML = sum;
-		} else {
-			document.getElementById('comments').innerHTML =
-				'There are only ten pins!';
+			if (isSpare) {
+				p1Score = p1Score + rollsArr[0];
+				document.getElementById('marker' + (count1 - 1)).innerHTML = p1Score;
+			}
+
+			if (rollsArr[0] + rollsArr[1] < 10) {
+				document.getElementsByClassName('p1Frames2')[count1].innerHTML =
+					rollsArr[1];
+				p1Score += rollsArr[0] + rollsArr[1];
+				document.getElementById('marker' + count1).innerHTML = p1Score;
+				document.getElementById('comments').innerHTML = '';
+				document.getElementById('buttons2').style.display = 'inline-block';
+				document.getElementById('buttons1').style.display = 'none';
+				count1++;
+				rollsArr = [];
+			} else if (rollsArr[0] + rollsArr[1] == 10) {
+				document.getElementsByClassName('p1Frames2')[count1].innerHTML = '/';
+				p1Score += rollsArr[0] + rollsArr[1];
+				document.getElementById('marker' + count1).innerHTML = p1Score;
+				document.getElementById('comments').innerHTML = 'Spare!';
+				document.getElementById('buttons2').style.display = 'inline-block';
+				document.getElementById('buttons1').style.display = 'none';
+				isSpare = true;
+				count1++;
+				rollsArr = [];
+			} else {
+				document.getElementById('comments').innerHTML =
+					'There are only ten pins!';
+				rollsArr.pop();
+				return;
+			}
+		}
+		if (rollsArr[0] == 10) {
+			document.getElementsByClassName('p1Frames1')[count1].innerHTML = pins;
+			document.getElementsByClassName('p1Frames2')[count1].innerHTML = 'X';
+			p1Score += 10;
+			document.getElementById('comments').innerHTML = 'Strike!';
+			document.getElementById('marker' + count1).innerHTML = p1Score;
+			document.getElementById('buttons2').style.display = 'inline-block';
+			document.getElementById('buttons1').style.display = 'none';
 			rollsArr = [];
+			isStrike = true;
+			count1++;
+		} else if (isStrike == false) {
+			return;
+		}
+	}
+}
+
+function playerTwoGame(pinsHit) {
+	let pins = pinsHit;
+
+	if (count2 < 10) {
+		rollsArr.push(pins);
+		document.getElementsByClassName('p2Frames1')[count2].innerHTML =
+			rollsArr[0];
+
+		if (rollsArr.length == 2) {
+			if (isStrike) {
+				p2Score = p2Score + rollsArr[0] + rollsArr[1];
+				document.getElementById('p2marker' + (count2 - 1)).innerHTML = p2Score;
+			}
+			if (isSpare) {
+				p2Score = p2Score + rollsArr[0];
+				document.getElementById('p2marker' + (count2 - 1)).innerHTML = p2Score;
+			}
+
+			if (rollsArr[0] + rollsArr[1] < 10) {
+				document.getElementsByClassName('p2Frames2')[count2].innerHTML =
+					rollsArr[1];
+				p2Score += rollsArr[0] + rollsArr[1];
+				document.getElementById('p2marker' + count2).innerHTML = p2Score;
+				document.getElementById('comments').innerHTML = '';
+				document.getElementById('buttons2').style.display = 'none';
+				document.getElementById('buttons1').style.display = 'inline-block';
+				count2++;
+				rollsArr = [];
+			} else if (rollsArr[0] + rollsArr[1] == 10) {
+				document.getElementsByClassName('p2Frames2')[count2].innerHTML = '/';
+				p2Score += rollsArr[0] + rollsArr[1];
+				document.getElementById('p2marker' + count2).innerHTML = p2Score;
+				document.getElementById('comments').innerHTML = 'Spare!';
+				document.getElementById('buttons2').style.display = 'none';
+				document.getElementById('buttons1').style.display = 'inline-block';
+				isSpare = true;
+				count2++;
+				rollsArr = [];
+			} else {
+				document.getElementById('comments').innerHTML =
+					'There are only ten pins!';
+				rollsArr.pop();
+				return;
+			}
+		}
+		if (rollsArr[0] == 10) {
+			document.getElementsByClassName('p2Frames1')[count2].innerHTML = pins;
+			document.getElementsByClassName('p2Frames2')[count2].innerHTML = 'X';
+			p2Score += 10;
+			document.getElementById('comments').innerHTML = 'Strike!';
+			document.getElementById('p2marker' + count2).innerHTML = p2Score;
+			document.getElementById('buttons2').style.display = 'none';
+			document.getElementById('buttons1').style.display = 'inline-block';
+			rollsArr = [];
+			isStrike = true;
+			count2++;
+		} else if (isStrike == false) {
 			return;
 		}
 	} else {
+		document.getElementById('buttons2').style.display = 'none';
+		document.getElementById('buttons1').style.display = 'none';
+		document.getElementById('comments').innerHTML = 'Game Over!';
 		return;
 	}
-	document.getElementById('marker' + count1).innerHTML = pinHit(
-		rollsArr[0],
-		rollsArr[1],
-	);
-	document.getElementById('buttons2').style.display = 'inline-block';
-	document.getElementById('buttons1').style.display = 'none';
-	count1++;
-	rolls = tempNum;
 }
 
-function playerTwoGame() {
-	let pins = pinsHit;
-	let tempNum = rolls;
+function getSpare(player) {
+	if (player == p1.getPlayerName()) {
+		p1Score = p1Score + rollsArr[0];
+		document.getElementById('marker' + (count1 - 1)).innerHTML = p1Score;
+	} else {
+		p2Score = p2Score + rollsArr[0];
+		document.getElementById('p2marker' + (count2 - 1)).innerHTML = p2Score;
+	}
+}
 
-	rollsArr.push(pins);
+function roll(pinsHit = 0) {
+	rollsArr.push(pin);
+	document.getElementsByClassName('p1Frames1')[count1].innerHTML = rollsArr[0];
+
 	if (rollsArr.length == 2) {
-		if (rollsArr[0] + rollsArr[1] <= 10) {
-			for (var x = 0; x < 20; x++) {
-				for (var frameIndex = 0; frameIndex < 2; frameIndex++) {
-					p1frames[frameIndex].innerHTML = rollsArr[frameIndex];
-				}
-			}
-			sum += pinHit(rollsArr[0], rollsArr[1]);
-			document.getElementById('marker' + count1).innerHTML = sum;
+		if (isStrike) {
+			p1Score = p1Score + rollsArr[0] + rollsArr[1];
+			document.getElementById('marker' + (count1 - 1)).innerHTML = p1Score;
+		}
+		if (isSpare) {
+			p1Score = p1Score + rollsArr[0];
+			document.getElementById('marker' + (count1 - 1)).innerHTML = p1Score;
+		}
+
+		if (rollsArr[0] + rollsArr[1] < 10) {
+			document.getElementsByClassName('p1Frames2')[count1].innerHTML =
+				rollsArr[1];
+			p1Score += rollsArr[0] + rollsArr[1];
+			document.getElementById('marker' + count1).innerHTML = p1Score;
+			document.getElementById('comments').innerHTML = '';
+			document.getElementById('buttons2').style.display = 'inline-block';
+			document.getElementById('buttons1').style.display = 'none';
+			count1++;
+			rollsArr = [];
+		} else if (rollsArr[0] + rollsArr[1] == 10) {
+			document.getElementsByClassName('p1Frames2')[count1].innerHTML = '/';
+			p1Score += rollsArr[0] + rollsArr[1];
+			document.getElementById('marker' + count1).innerHTML = p1Score;
+			document.getElementById('comments').innerHTML = 'Spare!';
+			document.getElementById('buttons2').style.display = 'inline-block';
+			document.getElementById('buttons1').style.display = 'none';
+			isSpare = true;
+			count1++;
+			rollsArr = [];
 		} else {
 			document.getElementById('comments').innerHTML =
 				'There are only ten pins!';
-			rollsArr = [];
+			rollsArr.pop();
 			return;
 		}
-	} else {
+	}
+	if (rollsArr[0] == 10) {
+		document.getElementsByClassName('p1Frames1')[count1].innerHTML = pins;
+		document.getElementsByClassName('p1Frames2')[count1].innerHTML = 'X';
+		p1Score += 10;
+		document.getElementById('comments').innerHTML = 'Strike!';
+		document.getElementById('marker' + count1).innerHTML = p1Score;
+		document.getElementById('buttons2').style.display = 'inline-block';
+		document.getElementById('buttons1').style.display = 'none';
+		rollsArr = [];
+		isStrike = true;
+		count1++;
+	} else if (isStrike == false) {
 		return;
 	}
-	document.getElementById('marker' + count1).innerHTML = pinHit(
-		rollsArr[0],
-		rollsArr[1],
-	);
-	document.getElementById('buttons2').style.display = 'none';
-	document.getElementById('buttons1').style.display = 'inline-block';
-	count1++;
 }
 
-let rollsArr = [];
+function playerTwo(number1, number2, rolls) {
+	// p2.roll(number1);
+	// p2.roll(number2);
+	p2rollMany(rollsArr, rolls);
+	console.log(p2.score());
 
-function pinHit(number1, number2) {
-	game.roll(number1);
-	game.roll(number2);
-	rollMany(0, --rolls);
-
-	return game.score();
+	return p2.score();
 }
 
-function rollMany(pins, rolls) {
+function p1rollMany(pins, rolls) {
 	for (var i = 0; i < rolls; i++) {
-		game.roll(pins);
+		p1.roll(pins[i]);
+	}
+}
+
+function p2rollMany(pins, rolls) {
+	for (var i = 0; i < rolls; i++) {
+		p2.roll(pins[i]);
 	}
 }
