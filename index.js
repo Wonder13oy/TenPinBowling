c9bfdcf44d63410deaad40af019f6839d61b40c7
Wandile Nxumalo <46021425+Wonder13oy@users.noreply.github.com>
2019-05-06 14:06:16 +0200

    Update index.js
    Commenting the index.js file

--- index.js
+++ index.js
@@ -7,12 +7,15 @@ let p1rolls = 2;
 let p2rolls = 2;
 let p1Score = 0;
 let p2Score = 0;
+//Since there is no score registered yet
 let isStrike = false;
 let isSpare = false;
 
+//Asking for the user names
 p1.setPlayerName(prompt('Enter Name Player One:'));
 p2.setPlayerName(prompt('Enter Name Player Two:'));
 
+//This is function is called when the start button is clicked
 function playGame() {
 	document.getElementById('playerOneName').innerText = p1.getPlayerName();
 	document.getElementById('playerTwoName').innerText = p2.getPlayerName();
@@ -22,6 +25,7 @@ function playGame() {
 	document.getElementById('startBut').style.display = 'none';
 }
 
+//Function takes the number input and calculates the score
 function playerOneGame(pinsHit) {
 	let pins = pinsHit;
 
@@ -29,17 +33,18 @@ function playerOneGame(pinsHit) {
 		rollsArr.push(pins);
 		document.getElementsByClassName('p1Frames1')[count1].innerHTML =
 			rollsArr[0];
-
+		
 		if (rollsArr.length == 2) {
-			if (isStrike) {
+			if (isStrike) {//Allocates the next 2 frame scores to the previous frame score
 				p1Score = p1Score + rollsArr[0] + rollsArr[1];
 				document.getElementById('marker' + (count1 - 1)).innerHTML = p1Score;
 			}
-			if (isSpare) {
+			if (isSpare) {//Allocates the next frame score to the previous frame score
 				p1Score = p1Score + rollsArr[0];
 				document.getElementById('marker' + (count1 - 1)).innerHTML = p1Score;
 			}
-
+			
+			//Checks the case whereby there is an openframe
 			if (rollsArr[0] + rollsArr[1] < 10) {
 				document.getElementsByClassName('p1Frames2')[count1].innerHTML =
 					rollsArr[1];
@@ -50,7 +55,7 @@ function playerOneGame(pinsHit) {
 				document.getElementById('buttons1').style.display = 'none';
 				count1++;
 				rollsArr = [];
-			} else if (rollsArr[0] + rollsArr[1] == 10) {
+			} else if (rollsArr[0] + rollsArr[1] == 10) {//When a spare is hit
 				document.getElementsByClassName('p1Frames2')[count1].innerHTML = '/';
 				p1Score += rollsArr[0] + rollsArr[1];
 				document.getElementById('marker' + count1).innerHTML = p1Score;
@@ -60,14 +65,14 @@ function playerOneGame(pinsHit) {
 				isSpare = true;
 				count1++;
 				rollsArr = [];
-			} else {
+			} else {//Triggered when the player gives an invalid frame
 				document.getElementById('comments').innerHTML =
 					'There are only ten pins!';
 				rollsArr.pop();
 				return;
 			}
 		}
-		if (rollsArr[0] == 10) {
+		if (rollsArr[0] == 10) {//When a strike is achieved
 			document.getElementsByClassName('p1Frames1')[count1].innerHTML = pins;
 			document.getElementsByClassName('p1Frames2')[count1].innerHTML = 'X';
 			p1Score += 10;
@@ -84,6 +89,7 @@ function playerOneGame(pinsHit) {
 	}
 }
 
+//The exact repeat of playerOneGame(pinsHit) but only for the second player
 function playerTwoGame(pinsHit) {
 	let pins = pinsHit;
 
@@ -148,93 +154,5 @@ function playerTwoGame(pinsHit) {
 		document.getElementById('buttons1').style.display = 'none';
 		document.getElementById('comments').innerHTML = 'Game Over!';
 		return;
-	}
-}
-
-function getSpare(player) {
-	if (player == p1.getPlayerName()) {
-		p1Score = p1Score + rollsArr[0];
-		document.getElementById('marker' + (count1 - 1)).innerHTML = p1Score;
-	} else {
-		p2Score = p2Score + rollsArr[0];
-		document.getElementById('p2marker' + (count2 - 1)).innerHTML = p2Score;
-	}
-}
-
-function roll(pinsHit = 0) {
-	rollsArr.push(pin);
-	document.getElementsByClassName('p1Frames1')[count1].innerHTML = rollsArr[0];
-
-	if (rollsArr.length == 2) {
-		if (isStrike) {
-			p1Score = p1Score + rollsArr[0] + rollsArr[1];
-			document.getElementById('marker' + (count1 - 1)).innerHTML = p1Score;
-		}
-		if (isSpare) {
-			p1Score = p1Score + rollsArr[0];
-			document.getElementById('marker' + (count1 - 1)).innerHTML = p1Score;
-		}
-
-		if (rollsArr[0] + rollsArr[1] < 10) {
-			document.getElementsByClassName('p1Frames2')[count1].innerHTML =
-				rollsArr[1];
-			p1Score += rollsArr[0] + rollsArr[1];
-			document.getElementById('marker' + count1).innerHTML = p1Score;
-			document.getElementById('comments').innerHTML = '';
-			document.getElementById('buttons2').style.display = 'inline-block';
-			document.getElementById('buttons1').style.display = 'none';
-			count1++;
-			rollsArr = [];
-		} else if (rollsArr[0] + rollsArr[1] == 10) {
-			document.getElementsByClassName('p1Frames2')[count1].innerHTML = '/';
-			p1Score += rollsArr[0] + rollsArr[1];
-			document.getElementById('marker' + count1).innerHTML = p1Score;
-			document.getElementById('comments').innerHTML = 'Spare!';
-			document.getElementById('buttons2').style.display = 'inline-block';
-			document.getElementById('buttons1').style.display = 'none';
-			isSpare = true;
-			count1++;
-			rollsArr = [];
-		} else {
-			document.getElementById('comments').innerHTML =
-				'There are only ten pins!';
-			rollsArr.pop();
-			return;
-		}
-	}
-	if (rollsArr[0] == 10) {
-		document.getElementsByClassName('p1Frames1')[count1].innerHTML = pins;
-		document.getElementsByClassName('p1Frames2')[count1].innerHTML = 'X';
-		p1Score += 10;
-		document.getElementById('comments').innerHTML = 'Strike!';
-		document.getElementById('marker' + count1).innerHTML = p1Score;
-		document.getElementById('buttons2').style.display = 'inline-block';
-		document.getElementById('buttons1').style.display = 'none';
-		rollsArr = [];
-		isStrike = true;
-		count1++;
-	} else if (isStrike == false) {
-		return;
-	}
-}
-
-function playerTwo(number1, number2, rolls) {
-	// p2.roll(number1);
-	// p2.roll(number2);
-	p2rollMany(rollsArr, rolls);
-	console.log(p2.score());
-
-	return p2.score();
-}
-
-function p1rollMany(pins, rolls) {
-	for (var i = 0; i < rolls; i++) {
-		p1.roll(pins[i]);
-	}
-}
-
-function p2rollMany(pins, rolls) {
-	for (var i = 0; i < rolls; i++) {
-		p2.roll(pins[i]);
 	}
 }
